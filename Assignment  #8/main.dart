7f1f2f5b3407070fe7830d543bb94af3275b59f8
FarazMubin234 <farazmubin96@gmail.com>
2024-08-12 04:17:53 +0500

    Assignment NO 8

---
+++ Assignment  #8/main.dart
@@ -0,0 +1,22 @@
+void main() {
+  int x = 5;
+  int y = 8;
+
+ if ((++x > y--) && (++x < ++y)){
+  print("condition true");
+ }
+  else {
+    print("condition false");
+  }
+  print(x);
+  print(y);
+
+   var list1 = [10,11,12,13,14,15];  
+   print(list1);   
+
+  for (int i = 1; i <= 10; i++) {
+    print('Number $i');
+}
+  List Names = ["faraz","shaheryar","ayan"];
+  print("$Names");
+}
